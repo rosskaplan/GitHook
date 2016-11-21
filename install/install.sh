1c92db969f2a8b6b45b16a091ae76d27098d54c4
brendabrandy <so@cooper.edu>
2016-11-21 16:39:32 -0500

    cleaned up post-commit

--- install/install.sh
+++ install/install.sh
@@ -13,24 +13,17 @@ mv .git .wiki
 mv .temp .git
 rm -rf .temp
 
-<<<<<<< HEAD
 git remote add origin $repo_link
 
 git --git-dir=.wiki add Home.md
 git --git-dir=.wiki commit -m "first commit"
 git --git-dir=.wiki remote add wiki $new_link
-=======
-git --git-dir=.wiki remote add wiki new_link
->>>>>>> 336e7f0593f9ed2bd5517414453395a1e20e26ea
 
 # Step 3 : Set up post-commit into .git
 mv post-commit .git/hooks/post-commit
 chmod +x .git/hooks/post-commit
-<<<<<<< HEAD
 
 # Step 4 : Populate the database with all the necessary information, including
 # repo name, username etc
 
 ./setup -u "$GIT_AUTHOR_EMAIL" -e "$GIT_AUTHOR_NAME"
-=======
->>>>>>> 336e7f0593f9ed2bd5517414453395a1e20e26ea
