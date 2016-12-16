0af62a06b8a45bab2b238bc8454be3376df8c521
brendabrandy <so@cooper.edu>
2016-12-16 11:16:09 -0500

    made formateWiki.cpp

--- scripts/changePage.cpp
+++
@@ -1,2 +0,0 @@
-
-    ofstream outfile()
---
+++ scripts/utils.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <string.h>
+#include <typeinfo>
+#include "creds.h"
+#include "utils.h"
+
+#include <mysql/mysql.h>
+
+using namespace std;
+
+void finish_with_error(MYSQL *con, string s){
+    cerr << "Error when " << s << " : " << mysql_error(con) << endl;
+    mysql_close(con);
+    exit(1);
+}
+
+string hasAccess(MYSQL *con, string username, string repourl){
+    string q = "SELECT DISTINCT R.rid FROM users U, works_in WI, repo R WHERE R.rurl='"+repourl+"' and U.uname='"+username+"' and U.uname=WI.uname and WI.rid=R.rid";
+    MYSQL_ROW row;
+    string access = "";
+    if (mysql_query(con, q.c_str()) != 0){
+        finish_with_error(con, "querying the database");
+    }
+    MYSQL_RES * result = mysql_store_result(con);
+    if (result == NULL){
+        finish_with_error(con, "obtaining result");
+    }
+    if (mysql_num_rows(result) != 0){
+        row = mysql_fetch_row(result);
+        access = (string)row[0];
+    }
+    return access;
+}
+
+bool existDB(MYSQL *con, string repo_link){
+    string q = "SELECT DISTINCT R.rid FROM repo R where R.rurl='"+repo_link+"'";
+    MYSQL_ROW row;
+    bool exist;
+    if (mysql_query(con, q.c_str()) != 0){
+        finish_with_error(con, "querying the database");
+    }
+    MYSQL_RES * result = mysql_store_result(con);
+    if (result == NULL){
+       finish_with_error(con, "obtaining result"); 
+    }
+
+    if (mysql_num_rows(result) == 0) return false;
+    return true;
+}
+
+bool existUser(MYSQL *con, string uname){
+    string q = "SELECT DISTINCT U.uname FROM users U where U.uname='"+uname+"';";
+    cout << q << endl;
+    MYSQL_ROW row;
+    bool exist;
+    if (mysql_query(con, q.c_str()) != 0){
+        finish_with_error(con, "querying the database");
+    }
+    MYSQL_RES *result = mysql_store_result(con);
+    if (result == NULL){
+        finish_with_error(con, "obtaining result");
+    }
+    cout << mysql_num_rows(result) << endl;
+    if (mysql_num_rows(result) == 0) return false;
+    return true;
+}
+
+
+
+
